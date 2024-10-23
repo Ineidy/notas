5f2ee08de7dbbfdfa7ed6d887b2a7f95941d2623
Ineidy <160775201+Ineidy@users.noreply.github.com>
2024-10-23 06:44:53 -0500

    feat: :sparkles: conexion a mongo

---
+++ server/helper/db.js
@@ -0,0 +1,33 @@
+import { MongoClient } from 'mongodb';
+
+    export class connect{
+        conection;
+        db;
+        static instanceConnect;
+        constructor(){
+            if (connect.instanceConnect)return instanceConnect;
+            connect.instanceConnect = this;
+            return connect.instanceConnect
+        }
+        async open(){
+            const uri = `mongodb://mongo:aUOKhWWBjUpuadetjYuuaojcthHNVyFN@autorack.proxy.rlwy.net:29641`
+            
+            try {
+                this.conection= new MongoClient(uri)
+                await this.conection.connect()
+                this.db = this.conection.db("notas");
+                this.dbAdmin = this.conection.db('admin')
+                this.conection = {status: 200, message: "Conectado"}
+                console.log(this.conection.message);
+            } catch (error) {
+                this.conection = {status: 400, message: "Error en la uri"}
+                console.log(this.conection.message);
+            }
+        }
+    }
+    
+    (async () => {
+
+        const connectionInstance = new connect();
+        await connectionInstance.open();
+    })();
