e544913ec48f7bb81b5e82016e29075c46a25d3e
Ineidy <160775201+Ineidy@users.noreply.github.com>
2024-10-29 07:32:59 -0500

    feat: :sparkles: configuracion vite

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,13 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'cert.pem')),
-    },
-    host: '0.0.0.0', // Esto permite que el servidor escuche en todas las interfaces
-    port: 3000,      // Cambia el puerto según tus preferencias
-  },
+  base: "https://ineidy.github.io/gestionHospitalaria/"
+//  server: {
+//    https: {
+//      key: fs.readFileSync(path.resolve(__dirname, 'key.pem')),
+//      cert: fs.readFileSync(path.resolve(__dirname, 'cert.pem')),
+//    },
+//    host: '0.0.0.0', // Esto permite que el servidor escuche en todas las interfaces
+//    port: 3000,      // Cambia el puerto según tus preferencias
+//  },
 })
